dbd371b13a718c63cee15a57bdcfa4eae4121d82
Joao Moreno <mail@joaomoreno.com>
2015-11-12 23:20:25 -0800

    first steps

--- extension.ts
+++ extension.ts
@@ -1,24 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'; 
+import { Range, Position, TextEditor, ExtensionContext, commands, window } from 'vscode'; 
+import { indentMode, parenMode, IPosition } from './parinfer';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+function fromEditorPosition(editorPosition: Position): IPosition {
+	return { row: editorPosition.line, column: editorPosition.character }; 
+}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-parinfer" is now active!'); 
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+export function activate(context: ExtensionContext) {
+	console.log('activated!');
 	
-	context.subscriptions.push(disposable);
+	function parinfer() {
+		const editor = window.activeTextEditor;
+		const document = editor.document;
+		const input = document.getText();
+		const output = indentMode(input, fromEditorPosition(editor.selection.active));
+		
+		if (typeof output !== 'string') {
+			console.log('got from parinfer:', output);
+			return;
+		}
+		
+		const range = new Range(new Position(0, 0), editor.document.positionAt(input.length));
+		editor.edit(builder => builder.replace(range, output));
+		console.log('success');
+	}
+
+	var disposable = commands.registerCommand('extension.sayHello', parinfer);
+	
+	window.onDidChangeTextEditorSelection(e => {
+		const document = e.textEditor.document;
+		if (!/\.clj$/.test(document.fileName)) {
+			return;
+		}
+		
+		parinfer();
+	}, context.subscriptions);
+	
+	// window.onDidChangeActiveTextEditor(onEditor, context.subscriptions);
 }
---
+++ parinfer.d.ts
@@ -0,0 +1,7 @@
+export interface IPosition {
+	row: number;
+	column: number;
+}
+
+export function indentMode(contents: string, cursor: IPosition);
+export function parenMode(contents: string, cursor?: IPosition);
