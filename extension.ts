193cfcb465cf544a942cdf66bc7fcdc5a2a8d530
Joao Moreno <jomo@microsoft.com>
2015-11-18 14:45:06 +0100

    remove log message

--- extension.ts
+++ extension.ts
@@ -1,8 +1,8 @@
 import {
 	Range, Position, TextEditor, ExtensionContext,
 	commands, window, StatusBarAlignment,
-	TextEditorSelectionChangeEvent 
-} from 'vscode'; 
+	TextEditorSelectionChangeEvent
+} from 'vscode';
 import { indentMode, parenMode, IPosition } from './parinfer';
 
 enum Mode {
@@ -22,8 +22,8 @@ function fromEditorPosition(editorPosition: Position): IPosition {
 	if (!editorPosition) {
 		return null;
 	}
-	
-	return { row: editorPosition.line, column: editorPosition.character }; 
+
+	return { row: editorPosition.line, column: editorPosition.character };
 }
 
 function shouldRun(fileName: string): boolean {
@@ -32,10 +32,10 @@ function shouldRun(fileName: string): boolean {
 
 export function activate(context: ExtensionContext) {
 	let modes: { [uri: string]: Mode; } = Object.create(null);
-	
+
 	const statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right);
 	statusBarItem.show();
-	
+
 	function render(editor: TextEditor) {
 		const uri = editor.document.uri.toString();
 		statusBarItem.text = modes[uri] === Mode.Indent ? 'Indent' : 'Paren';
@@ -46,47 +46,46 @@ export function activate(context: ExtensionContext) {
 		modes[uri] = modes[uri] === Mode.Indent ? Mode.Paren : Mode.Indent;
 		render(window.activeTextEditor);
 	}
-	
+
 	function parinfer(editor: TextEditor, position: Position = null) {
 		const document = editor.document;
 		const uri = document.uri.toString();
 		const input = document.getText();
 		const fn = (modes[uri] === Mode.Indent && position) ? indentMode : parenMode;
 		const output = position ? fn(input, fromEditorPosition(position)) : fn(input);
-		
+
 		if (typeof output !== 'string') {
-			console.log('got from parinfer:', output);
 			return;
 		}
-		
+
 		const range = new Range(new Position(0, 0), document.positionAt(input.length));
 		editor.edit(builder => builder.replace(range, output));
 	}
-	
+
 	const eventuallyParinfer = debounce(parinfer, 50);
-	
+
 	function onSelectionChange({ textEditor }: TextEditorSelectionChangeEvent) {
 		if (!shouldRun(textEditor.document.fileName)) {
 			return;
 		}
-		
+
 		eventuallyParinfer(textEditor, textEditor.selection.active);
 	}
-	
+
 	function onEditorChange(editor: TextEditor) {
 		if (!shouldRun(editor.document.fileName)) {
 			return;
 		}
-		
+
 		parinfer(editor);
 		render(editor);
 	}
-	
+
 	context.subscriptions.push(
 		commands.registerCommand('parinfer.toggleMode', toggleMode),
 		window.onDidChangeTextEditorSelection(onSelectionChange),
 		window.onDidChangeActiveTextEditor(onEditorChange)
 	);
-	
+
 	render(window.activeTextEditor);
 }
