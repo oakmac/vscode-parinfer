268c367dba7238fb8d576e0e24ba5a8cc4fb68dc
Chris Oakman <chris@oakmac.com>
2018-08-03 23:51:08 -0500

    formatting

--- src/messages.ts
+++ src/messages.ts
@@ -1,13 +1,19 @@
-export function parenModeFailedMsg(currentFile: string) {
-	return (
-`Parinfer was unable to parse ${currentFile}.
- It is likely that this file has unbalanced parentheses and will not compile.
- Parinfer will enter Paren Mode so you may fix the problem. Press Ctrl + ( to switch to Indent Mode once the file is balanced.`)
+export function parenModeFailedMsg (currentFile) {
+  return (
+    'Parinfer was unable to parse ' + currentFile + ' ' +
+    'It is likely that this file has unbalanced parentheses and will not compile. ' +
+    'Parinfer will enter Paren Mode so you may fix the problem. ' +
+    'Press Ctrl + ( to switch to Indent Mode once the file is balanced.'
+  )
 }
 
-export function parenModeChangedFileMsg(currentFile: string, diff: number) {
-	return (
-`Parinfer needs to make some changes to ${currentFile} before enabling Indent Mode. These changes will only affect whitespace and indentation; the structure of the file will be unchanged.
- ${diff} ${diff === 1 ? "line" : "lines"} will be affected.
- Would you like Parinfer to modify the file? (recommended)`);
+export function parenModeChangedFileMsg (currentFile, diff) {
+  const lines = (diff === 1 ? 'line' : 'lines')
+
+  return (
+    'Parinfer needs to make some changes to ' + currentFile + ' before enabling Indent Mode. ' +
+    'These changes will only affect whitespace and indentation; the structure of the file will be unchanged. ' +
+    diff + ' ' + lines + ' will be affected. ' +
+    'Would you like Parinfer to modify the file? (recommended)'
+  )
 }
