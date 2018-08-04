a3aa3035305666b0de5546ee39e878ea8dd340e3
Chris Oakman <chris@oakmac.com>
2018-08-03 23:16:44 -0500

    cleanup; formatting

--- src/extension.ts
+++ src/extension.ts
@@ -1,63 +1,53 @@
-import {
-	Range,
-	Position,
-	Selection,
-	TextEditor,
-	ExtensionContext,
-	commands,
-	window,
-	workspace
-} from "vscode";
+import { commands, window } from 'vscode'
+import { initStatusBar, updateStatusBar } from './statusbar'
+import { editorStates } from './editor'
+import { disableParinfer, applyParinfer, parinfer } from './parinfer'
 
-import { EditorStates, EditorState } from "editor";
-import { initStatusBar, updateStatusBar } from "./statusbar";
-import { editorStates } from "./editor";
-import { disableParinfer, applyParinfer, parinfer } from "./parinfer";
+editorStates.addWatch((states) => {
+  const editor = window.activeTextEditor
+  const currentEditorState = states.get(editor)
 
-editorStates.addWatch((states: EditorStates) => {
-	const editor = window.activeTextEditor;
-	const currentEditorState = states.get(editor);
+  if (editor && currentEditorState) {
+    updateStatusBar(currentEditorState)
+    if (currentEditorState === 'indent-mode' || currentEditorState === 'paren-mode') {
+      applyParinfer(editor)
+    }
+  } else if (editor) {
+    updateStatusBar()
+  }
+})
 
-	if (editor && currentEditorState) {
-		updateStatusBar(currentEditorState);
-		if (currentEditorState === "indent-mode" ||
-		    currentEditorState === "paren-mode") {
-			applyParinfer(editor);
-		}
-	} else if (editor) {
-		updateStatusBar();
-	}
-});
-
-function toggleMode(editor: TextEditor) {
-	editorStates.update((states: EditorStates) => {
-		const nextState: EditorState  = states.get(editor) === "paren-mode" ? "indent-mode" : "paren-mode";
-		return states.set(editor, nextState);
-	});
+function toggleMode (editor) {
+  editorStates.update((states) => {
+    const nextState = states.get(editor) === 'paren-mode' ? 'indent-mode' : 'paren-mode'
+    return states.set(editor, nextState)
+  })
 }
 
-function activatePane(editor?: TextEditor) {
-	if (editor) {
-		parinfer(editor);
-	}
+function activatePane (editor) {
+  if (editor) {
+    parinfer(editor)
+  }
 }
 
-export function activate(context: ExtensionContext) {
+export function activate (context) {
+  // TODO: put the version here
+  console.log('vscode-parinfer activated')
 
-	initStatusBar("parinfer.toggleMode");
+  initStatusBar('parinfer.toggleMode')
 
-	activatePane(window.activeTextEditor);
+  activatePane(window.activeTextEditor)
 
-	context.subscriptions.push(
-		commands.registerCommand("parinfer.toggleMode", () => {
-			toggleMode(window.activeTextEditor);
-		}),
-		commands.registerCommand("parinfer.disable", () => {
-			disableParinfer(window.activeTextEditor);
-		}),
-		window.onDidChangeTextEditorSelection((event) => {
-			applyParinfer(window.activeTextEditor, event);
-		}),
-		window.onDidChangeActiveTextEditor(activatePane)
-	);
+  context.subscriptions.push(
+    commands.registerCommand('parinfer.toggleMode', () => {
+      toggleMode(window.activeTextEditor)
+    }),
+    commands.registerCommand('parinfer.disable', () => {
+      disableParinfer(window.activeTextEditor)
+    }),
+    window.onDidChangeTextEditorSelection((event) => {
+      applyParinfer(window.activeTextEditor, event)
+    }),
+    window.onDidChangeActiveTextEditor(activatePane)
+  )
 }
